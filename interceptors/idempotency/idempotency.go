206d338980f9de3f614f434adbec41bfac6879ae
Andrii Filenko <andrey.filenochko@gmail.com>
2024-10-11 14:13:04 +0300

    SDP-775: add new interceptor

---
+++ interceptors/idempotency/idempotency.go
@@ -0,0 +1,26 @@
+package idempotency
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/quarks-tech/protoevent-go/pkg/event"
+	"github.com/quarks-tech/protoevent-go/pkg/eventbus"
+)
+
+func SubscriberInterceptor(checker UniqueChecker) eventbus.SubscriberInterceptor {
+	return func(ctx context.Context, md *event.Metadata, e interface{}, handler eventbus.Handler) error {
+		uniq, err := checker(ctx, md.ID)
+		if err != nil {
+			return fmt.Errorf("checking event [%s] id [%s] idempotency: %w", md.Type, md.ID, err)
+		}
+
+		if !uniq {
+			return eventbus.NewUnprocessableEventError(fmt.Errorf("event [%s] with id[%s] is not unique", md.Type, md.ID))
+		}
+
+		return handler(ctx, e)
+	}
+}
+
+type UniqueChecker func(ctx context.Context, id string) (bool, error)
