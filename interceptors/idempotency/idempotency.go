d6ca00539e5c5ab3de5afe389271176c188fff1e
Andrii Filenko <andrey.filenochko@gmail.com>
2024-10-11 15:02:13 +0300

    SDP-775: add new interceptor

--- interceptors/idempotency/idempotency.go
+++ interceptors/idempotency/idempotency.go
@@ -9,31 +9,36 @@ import (
 	"github.com/quarks-tech/protoevent-go/pkg/eventbus"
 )
 
-func SubscriberInterceptor(st Store) eventbus.SubscriberInterceptor {
-	return func(ctx context.Context, md *event.Metadata, e interface{}, handler eventbus.Handler) error {
-		if err := st.MarkProcessing(ctx, md.ID); err != nil {
-			if errors.Is(err, ErrNotUnique) {
-				return eventbus.NewUnprocessableEventError(fmt.Errorf("event [%s] with id [%s] is not unique", md.Type, md.ID))
+func SubscriberInterceptor(st Storage) eventbus.SubscriberInterceptor {
+	return func(ctx context.Context, md *event.Metadata, e interface{}, handler eventbus.Handler) (err error) {
+		if err = st.Lock(ctx, md.Type, md.ID); err != nil {
+			if errors.Is(err, ErrEventReprocessing) {
+				return eventbus.NewUnprocessableEventError(fmt.Errorf("idempotency: event [%s] with id [%s] is not unique", md.Type, md.ID))
 			}
 
-			return fmt.Errorf("marking event [%s] id [%s] for processing: %w", md.Type, md.ID, err)
+			return fmt.Errorf("idempotency: marking event [%s] id [%s] for processing: %w", md.Type, md.ID, err)
 		}
 
-		if err := handler(ctx, e); err != nil {
+		defer func() {
+			err = st.Unlock(ctx, md.Type, md.ID)
+		}
+
+		if err = handler(ctx, e); err != nil {
 			return err
 		}
 
-		if err := st.MarkDone(ctx, md.ID); err != nil {
-			return fmt.Errorf("marking event [%s] id [%s] as done: %w", md.Type, md.ID, err)
+		if err = st.Done(ctx, md.Type, md.ID); err != nil {
+			return fmt.Errorf("idempotency: marking event [%s] id [%s] as done: %w", md.Type, md.ID, err)
 		}
 
 		return nil
 	}
 }
 
-var ErrNotUnique = fmt.Errorf("event not unique")
+var ErrEventReprocessing = fmt.Errorf("idempotency: event reprocessing is prohibited")
 
-type Store interface {
-	MarkProcessing(ctx context.Context, id string) error
-	MarkDone(ctx context.Context, id string) error
+type Storage interface {
+	Lock(ctx context.Context, eventType, id string) error
+	Unlock(ctx context.Context, eventType, id string) error
+	Done(ctx context.Context, eventType, id string) error
 }
