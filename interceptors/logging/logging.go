a3f6e89f5439aa4d871c3b7673c4f9deeffecb4b
Andrii Filenko <andrey.filenochko@gmail.com>
2025-06-16 15:31:07 +0300

    SDP-1060: add logging interceptor

--- interceptors/logging/logging.go
+++ interceptors/logging/logging.go
@@ -15,10 +15,9 @@ type Factory func(context.Context) Logger
 
 func LoggingInterceptor(factory Factory) eventbus.SubscriberInterceptor {
 	return func(ctx context.Context, md *event.Metadata, e any, handler eventbus.Handler) error {
-		iCtx := event.NewIncomingContext(ctx, md)
-		hErr := handler(iCtx, e)
+		hErr := handler(ctx, e)
 		if hErr != nil {
-			factory(iCtx).Errorf("error while handling event %s, %s %+v", md.Type, hErr.Error(), e)
+			factory(ctx).Errorf("error while handling event %s, %s %+v", md.Type, hErr.Error(), e)
 		}
 
 		return hErr
